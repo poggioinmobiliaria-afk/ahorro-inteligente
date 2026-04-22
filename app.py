c7134887c3c6e8d1f64fef64e76dd9de0488197f
poggioinmobiliaria-afk <poggioinmobiliaria@gmail.com>
2026-04-22 19:42:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -325,3 +325,27 @@ st.markdown("""
 </div>
 """, unsafe_allow_html=True)
           
+import requests
+
+def buscar_supertop(producto):
+    url = f"https://www.supertop.com.ar/api/catalog_system/pub/products/search/{producto}"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    resultados = []
+    
+    for item in data:
+        try:
+            nombre = item.get("productName")
+            precio = item.get("items")[0]["sellers"][0]["commertialOffer"]["Price"]
+            
+            resultados.append({
+                "nombre": nombre,
+                "precio": precio,
+                "supermercado": "SuperTop"
+            })
+        except:
+            pass
+    
+    return resultados
